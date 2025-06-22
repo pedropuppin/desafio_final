ad63ff33c3490d443d0f9553d6ac28944f2ede89
Pedro Puppin <puppin.pedro@gmail.com>
2025-06-22 16:26:29 -0300

    Refactor settings to load SECRET_KEY and DEBUG from environment variables

--- loja_online/settings.py
+++ loja_online/settings.py
@@ -11,19 +11,22 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p+iwf(06bl=mfef(^e$cy3_9x&5%im(jd-0(9ne6+ui@y!7dp)'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = []
 
